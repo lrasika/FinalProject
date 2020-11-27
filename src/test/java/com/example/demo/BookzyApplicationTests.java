96a548f6c457758ac1114d756dce7d6709a2bc68
student <block.kandy@gmail.com>
2020-11-27 22:35:21 +0000

    first commit

---
+++ src/test/java/com/example/demo/BookzyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookzyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
